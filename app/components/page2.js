3ab7512e440ad75110245ef563fe26227392ba6e
rajdev04 <rajdevshrivastava72@gmail.com>
2024-01-12 02:24:43 +0530

    getting started

--- app/components/page2.js
+++ app/components/page2.js
@@ -3,7 +3,12 @@ import React from 'react'
 const page2 = () => {
   return (
     <div className='cont-2'>
-        <div></div>
+        <div className='cont-2-part-1'>
+            <h1 className='cont-2-text-1'>Our Services</h1>
+        </div>
+        <div className='cont-2-part-2'>
+            <h1 className='cont-2-text-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate sem ut turpis facilisis, at faucibus erat tempor. Nulla in semper leo, elementum pharetra magna. Sed massa dui, consectetur id.</h1>
+        </div>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import '@/app/globals.css'
 import Link from 'next/link'
 import Nav from '@/app/components/nav'
 import Page1 from '@/app/components/page1'
+import Page2 from '@/app/components/page2'
 
 const page = () => {
   return (
@@ -10,6 +11,8 @@ const page = () => {
       <Nav></Nav>
       <br/><br/><br/><br/><br/>
       <Page1></Page1>
+      <br/>
+      <Page2></Page2>
     </>
   )
 }
