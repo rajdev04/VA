67768ca7b195b31d40364435c23d0fba94e67b15
rajdev04 <rajdevshrivastava72@gmail.com>
2024-01-12 02:07:35 +0530

    getting started

---
+++ app/components/nav.js
@@ -0,0 +1,48 @@
+'use client'
+import React from 'react'
+import '@/app/components/nav'
+import Link from 'next/link'
+import 'remixicon/fonts/remixicon.css'
+
+const nav = () => {
+  return (
+    <>
+        <div className='navbar'>
+            <div className='navleft'>
+                <img src='./VR2.png' className='logo'/>
+                <h1 className='nav-text'>Vaidik Anusthanam</h1>
+            </div>
+            <div className='navright'>
+
+                <div className='nav-icon-wrap'>  
+                    <div className='gg'> <i class="ri-home-smile-line"></i></div>
+                    <div className='gg'><Link className='hyperlink' href=''>Home</Link></div>
+                </div>
+
+                <div className='nav-icon-wrap'>
+                    <div className='gg'><i class="ri-ancient-pavilion-line"></i></div>
+                    <div className='gg'><Link className='hyperlink' href=''>About us</Link></div>
+                </div>
+
+                <div className='nav-icon-wrap'>
+                    <div className='gg'><i class="ri-gallery-line"></i></div>
+                    <div className='gg'><Link className='hyperlink' href=''>Gallery</Link></div>
+                </div>
+
+                <div className='nav-icon-wrap'>
+                    <div className='gg'><i class="ri-service-line"></i></div>
+                    <div className='gg'><Link className='hyperlink' href=''>Our Services</Link></div>
+                </div>
+
+                <div className='nav-icon-wrap'>
+                    <div className='gg'><i class="ri-contacts-line"></i></div>
+                    <div className='gg'><Link className='hyperlink' href=''>Contact us</Link></div>
+                </div>    
+
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default nav
---
+++ app/components/page1.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import '@/app/globals.css'
+
+const page1 = () => {
+  return (
+    <>
+        <div className='cont-1'>
+            <div className='cont-1-left'>
+                <h1 className='cont-1-text-1'>Welcome to</h1>
+                <h1 className='cont-1-text-2'>Vaidik Anusthanam</h1>
+                <h1 className='cont-1-text-3'>Your Initial Initiated Gateway for Spiritual Fulfillment!</h1>
+            </div>
+            <div className='cont-1-right'>
+                    <div className='random-test-1'>
+                    <div className='cont-1-img-1'>
+                        <img className='img-1-part-1' src='./group333.png'/>
+                    </div>
+                    <div className='cont-1-img-2'>
+                        <img className='img-1-part-2' src='./group1rerere.png'/>
+                    </div>
+                    <div className='cont-1-img-3'>
+                        <img className='img-1-part-3' src='./group111.png'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default page1
