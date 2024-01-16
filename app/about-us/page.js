8f8d4b89f8a3246cc2c8caef6f57b8ea9485edd2
rajdev04 <rajdevshrivastava72@gmail.com>
2024-01-16 17:38:40 +0530

    gg

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -20,7 +20,7 @@ const page = () => {
           <div className='about-us-container-1'>
             <div className='ab-left'>
             <div className='ab-left-img-cont'>
-                <img src='./yaadein75.jpg' className='ab-left-img'/>
+                <img src='./yaadein135.jpg' className='ab-left-img'/>
             </div>
         </div> 
         <div className='ab-right'>
@@ -46,7 +46,7 @@ const page = () => {
             <div className='about-us-container-2'>
             <div className='ab-left'>
             <div className='ab-left-img-cont'>
-                <img src='./yaadein62.jpg' className='ab-left-img'/>
+                <img src='./welocme.jpg' className='ab-left-img'/>
             </div>
         </div> 
         <div className='ab-right'>
@@ -76,6 +76,113 @@ const page = () => {
             <div className='ab-8'>
                 <h1>Pilgrimage Tourism</h1>
                 <h2>With us, you can easily embark on your spiritual journeys to the most revered destinations. Guided by our erudite, experienced team, you can blend with the divine in their sacred landscapes.</h2>
+            </div>
+            <div className='ab-12'>
+                <h1>At Vaidik Anushthanam, our dedication to adhering to authenticity initiates with implementing integrity, which speeds up your spiritual growth. Vouch for us on this inner transformative journey where ancient wisdom blends with modern convenience. Here, your spiritual seeking will surely find its true abode. Connect with us now to closely experience the divine within us and let your soul soak in abundance.</h1>
+            </div>
+        </div>
+            </div>
+
+
+
+            <div className='about-us-container-2'>
+            <div className='ab-left'>
+            <div className='ab-left-img-cont'>
+                <img src='./yaadein91.jpg' className='ab-left-img'/>
+            </div>
+        </div> 
+        <div className='ab-right'>
+            <div className='ab-1'>
+                <h1>WHY CHOOSE US?</h1>
+            </div>
+            <div className='ab-2'>
+                <h1>In a world swinging between chaos and choice, everybody carries a deep desire for liberation. Each one of us wants to be free. Free from what? Free from physical ailments, mental instability, emotional burdens, energy vampires, and financial insecurity. As spiritual seekers, we are left with two choices repeat or evolve!</h1>
+                <br/>
+                <h1>Selecting the former is a mundane choice, but opting for the latter is a prudent, aware decision that can change your life in various aspects. If you are travelling on a different, unknown terrain, opting for an experienced guide will be prudent, as it will reduce your time and mental burden. Similarly, for your spiritual evolution, selecting the right partner is paramount. This is where Vaidik Anushthanam stands out as your unrivaled choice for all your sacred needs. Our match is unparalleled because we provide,</h1>
+            </div>
+            <div className='ab-3'>
+                <h1>Authenticity and Tradition</h1>
+                <h2>As a preserver of aristocratic Sanatan Dharma, we are custodians of ancient Vedic traditions. Our rituals, ceremonies, and practices are steeped in authenticity that ensures you experience the true essence of spirituality.</h2>
+            </div>
+            <div className='ab-4'>
+                <h1>Expert Guidance</h1>
+                <h2>Our enriched team is composed of experienced priests, erudite astrologers, and top-notch numerologists who hold decade-old experience in their respective fields. Their expertise will surely leverage your spiritual path.</h2>
+            </div>
+            <div className='ab-5'>
+                <h1>Global Reach</h1>
+                <h2>Regardless of your geographical location, we are there for you. Spiritual awakening knows no boundaries, and so we are. With our elegant online services, you can easily access your spiritual rituals and can have guidance irrespective of your location.</h2>
+            </div>
+            <div className='ab-6'>
+                <h1>Transparency and Integrity</h1>
+                <h2>We take pride in our commitment to honesty and transparency. At Vaidik Anushthanam, there is a big no for any hidden agendas or unethical practices.</h2>
+            </div>
+            <div className='ab-7'>
+                <h1>Full Spectrum of Services</h1>
+                <h2>From Pooja to pilgrimage tours, astrology to numerology, we offer a comprehensive suite of services and ensure that all your spiritual needs are fulfilled under one roof.</h2>
+            </div>
+            <div className='ab-8'>
+                <h1>Mission-Driven</h1>
+                <h2>Our missions, which include global spiritual upliftment and empowering emerging Gurukul Brahmins, reflect our dedication to a much higher purpose beyond profit.</h2>
+            </div>
+            <div className='ab-9'>
+                <h1>Personalized Experience</h1>
+                <h2>We understand that spirituality is a deep internal journey. It is like diving under the depths of the inner ocean. Our services are specifically tailored to your unique needs which provides you a personalized and meaningful experience.</h2>
+            </div>
+            <div className='ab-10'>
+                <h1>Satisfaction Guaranteed</h1>
+                <h2>Our primary goal is your satisfaction with spiritual growth. We go the extra mile to ensure that you seek and find soulful solace, calmness and clarity with spiritual growth through our offerings.</h2>
+            </div>
+            <div className='ab-11'>
+                <h1>Global Community</h1>
+                <h2>Join a thriving community of spiritual seekers who share and aspire your spiritual upliftment. Connect, learn, and grow with quality souls from all around the world.</h2>
+            </div>
+            <div className='ab-11'>
+                <h1>Legacy of Trust</h1>
+                <h2>With a legacy of trust dating back decades, Vaidik Anushthanam has become synonymous with spirituality, reliability, and unwavering devotion to your well-being.</h2>
+            </div>
+            <div className='ab-12'>
+                <h1>By opting Vaidik Anushthanam as your spiritual companion, you soon realize that you are not a physical body on a spiritual journey, but rather a spiritual soul on a physical journey. This realization opens a portal of a transformative journey towards inner peace, enlightenment, and spiritual fulfilment. With us, your spiritual aspirations find their true abode and your path to the divine is enlightened with authenticity and grace.</h1>
+            </div>
+        </div>
+            </div>
+
+
+
+            <div className='about-us-container-2'>
+            <div className='ab-left'>
+            <div className='ab-left-img-cont'>
+                <img src='./yaadein75.jpg' className='ab-left-img'/>
+            </div>
+        </div> 
+        <div className='ab-right'>
+            <div className='ab-1'>
+                <h1>OUR GLORIOUS PURPOSE</h1>
+            </div>
+            <div className='ab-2'>
+                <h1>At Vaidik Anushthanam, our mission is a sacred summon which is fueled by a deep desire for spiritual enrichment and the betterment of humanity. Our unwavering dedication revolves around several core pillars, like</h1>
+            </div>
+            <div className='ab-3'>
+                <h1>Global Level Spiritual Elevation</h1>
+                <h2>We deeply aspire to elevate spirituality on a global scale that fosters unity, upliftment, and peace among all. Our sole mission is to spread the light of our ancient Vedic wisdom to every corner of the world which guides the seekers on their spiritual odyssey.</h2>
+            </div>
+            <div className='ab-4'>
+                <h1>Providing a Promising Platform to Emerging Gurukul Brahmins</h1>
+                <h2>We empower the next generation of spiritual leaders by offering them a promising platform to share their knowledge and talents. We believe in preserving and nurturing the invaluable traditions of the Gurukul system.</h2>
+            </div>
+            <div className='ab-5'>
+                <h1>Clarify the Devotees</h1>
+                <h2>We are here to provide clarity, guidance, and solace to those seeking spiritual enlightenment. Our mission is to remove the fog of uncertainty and lead devotees towards a path of inner wisdom and understanding.</h2>
+            </div>
+            <div className='ab-6'>
+                <h1>Birth, Life, and Death Sanskar</h1>
+                <h2>We uphold the sanctity of life's key milestones, ensuring that every ritual is performed with reverence and authenticity. Our mission is to facilitate these sacred ceremonies, honouring the divine journey from birth to death.</h2>
+            </div>
+            <div className='ab-7'>
+                <h1>Pure Organic Orthodox Pooja and Nothing Else</h1>
+                <h2>We are committed to authentic, transparent, and sincere practices that uphold the integrity of Vedic rituals. Our mission is to deliver pure and organic Poojas without any compromise on traditional values.</h2>
+            </div>
+            <div className='ab-12'>
+                <h1>At Vaidik Anushthanam, our mission is a sacred covenant, and we invite you to join us on this transformative journey towards spiritual enlightenment, global harmony, and the preservation of timeless traditions. Together, we illuminate the path to the divine and create a world of profound spiritual elevation and fulfillment.</h1>
             </div>
         </div>
             </div>
@@ -90,37 +197,40 @@ const page = () => {
         </div> 
         <div className='ab-right'>
             <div className='ab-1'>
-                <h1>WHAT WE OFFER</h1>
+                <h1>OUR ENVISION</h1>
+            </div>
+            <div className='ab-2'>
+                <h1>Our Vision Illuminating Paths to Spiritual Fulfillment
+                  <br/><br/>
+                At Vaidik Anushthanam, we envision a world where spirituality is not just a facet of life but the very essence of our existence. Our vision is a beacon of light that guides us forward on this sacred path
+                </h1>
             </div>
             <div className='ab-3'>
-                <h1>Online/Offline Pooja </h1>
-                <h2>With us, you can completely immerse yourself in various divine rituals from the comfort of your home, or you can opt to visit our sanctified spaces for any offline ceremonies that invoke blessings and positivity within you.</h2>
+                <h1>Global Spiritual Enlightenment</h1>
+                <h2>We see a world where individuals of all backgrounds and beliefs come together to seek higher spiritual truths. Our vision is to elevate spirituality on a global scale, fostering unity, understanding, and inner peace.</h2>
             </div>
             <div className='ab-4'>
-                <h1>Online/Offline Darshan </h1>
-                <h2>You can also witness the presence of the divine through our live streams or visit sacred shrines. With us, you can stay connected with the arcane presence from wherever you are. </h2>
+                <h1>Empowering Emerging Gurukul Brahmins </h1>
+                <h2>We aim to be a torchbearer for the Gurukul tradition, providing a promising platform for emerging gurukul Brahmins to share their wisdom and carry forward the legacy of our ancient traditions.</h2>
             </div>
             <div className='ab-5'>
-                <h1>Birth, Life, and Death Sanskars</h1>
-                <h2>We are a sacred friend in every stage of your life. From birth to childhood rituals, marriage to domestic daily rituals Sacredly performing last rites and even beyond, we constantly strive to guide you through all the rites that honor these pivotal moments of your life. </h2>
+                <h1>Clarity and Wisdom </h1>
+                <h2>We envision a world where seekers find clarity, guidance, and profound wisdom on their spiritual journeys. Our vision is to illuminate the path to inner enlightenment, helping individuals make informed choices and find solace.</h2>
             </div>
             <div className='ab-6'>
-                <h1>Astrology</h1>
-                <h2>With us, you can explore the profound ancient wisdom of Vedic astrology to gain broader insight related to various journeys of your life, your hidden destiny, your untapped potential, and much more. </h2>
+                <h1>Honoring Life's Sacred Milestones </h1>
+                <h2>We see a society that reveres and respects the sacred rituals of birth, life, and death. Our vision is to facilitate these essential ceremonies with authenticity and reverence, ensuring the continuity of timeless traditions.</h2>
             </div>
             <div className='ab-7'>
-                <h1>Numerology</h1>
-                <h2>We help you decrypt the hidden spiritual meanings behind your specific numbers and demystify their impact on your life with our erudite numerologists. </h2>
+                <h1>Purity and Integrity </h1>
+                <h2>We envision a spiritual landscape where every Pooja is conducted with organic authenticity, and where no cheating or compromise on tradition exists. Our vision is to uphold the sanctity of Vedic rituals and practices.</h2>
             </div>
-            <div className='ab-8'>
-                <h1>Pilgrimage Tourism</h1>
-                <h2>With us, you can easily embark on your spiritual journeys to the most revered destinations. Guided by our erudite, experienced team, you can blend with the divine in their sacred landscapes.</h2>
+            <div className='ab-12'>
+                <h1>In this vision, Vaidik Anushthanam stands as a guiding star, illuminating the path to the divine and offering a sanctuary for spiritual seekers worldwide. Together, we aspire to create a world where spirituality is not merely a choice but a way of life, where ancient traditions thrive, and where each soul finds its truest, most enlightened self.</h1>
             </div>
         </div>
             </div>
-
-
-
+            <br/>
         </div>
    </>
   )
