49eadad6daa138d37fda62e8741bdf8de2c2afb2
rajdev04 <rajdevshrivastava72@gmail.com>
2024-01-17 15:22:50 +0530

    gg

---
+++ app/regularpooja/page.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Nav from '@/app/components/nav'
+
+const page = () => {
+  return (
+    <>
+
+        <div className='nav-wraper'>
+          <Nav></Nav>
+        </div>
+
+        <div className='pooja-cont'>
+
+
+            <div className='service-cont'>
+                
+            </div>
+
+
+        </div>
+    </>
+  )
+}
+
+export default page
